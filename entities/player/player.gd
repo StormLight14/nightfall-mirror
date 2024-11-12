60107289210e4b3abc2de61685874ffeb7282323
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2024-11-12 14:50:10 -0700

    add player movement code, player sprite, and add start game button to menu

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,1 +1,9 @@
 extends CharacterBody2D
+
+const SPEED = 500.0
+
+func _physics_process(delta: float) -> void:
+	var h_dir := Input.get_axis("left", "right")
+	var v_dir := Input.get_axis("up", "down")
+	var direction := Vector2(h_dir, v_dir).normalized()
+	velocity = direction * SPEED * delta

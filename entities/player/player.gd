8655376a1dcf38c8d0bc5815de592cbe513446ac
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-13 16:11:55 -0700

    make vars constant type

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var SPEED := 3000.0
 
 func _physics_process(delta: float) -> void:
-	var input_vector = Input.get_vector("left", "right", "up", "down")
+	var input_vector := Input.get_vector("left", "right", "up", "down")
 	var direction := input_vector.normalized()
 	velocity = direction * SPEED * delta
 	move_and_slide()

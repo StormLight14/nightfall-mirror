53a56058136db3067be8ff84feb99b00b98eac56
stormyyy <stormyyy@noreply.codeberg.org>
2024-11-14 08:53:44 -0700

    create Items and Player globals

---
+++ globals/Items.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var items := {
+	"rock": {
+		"display_name": "Rock"
+	}
+}
---
+++ globals/Player.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var player_inventory := [
+	[null, null, null, null, null],
+	[null, null, null, null, null],
+	[null, null, null, null, null]
+]

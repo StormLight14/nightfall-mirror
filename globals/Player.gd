0f777000c210ffeaf34c33110f19e1dd3032dc2f
stormyyy <stormyyy@noreply.codeberg.org>
2024-11-14 08:58:07 -0700

    create add_to_inventory() function in Player global

--- globals/Player.gd
+++ globals/Player.gd
@@ -1,7 +1,18 @@
 extends Node
 
-var player_inventory := [
+var inventory := [
 	[null, null, null, null, null],
 	[null, null, null, null, null],
 	[null, null, null, null, null]
 ]
+
+func add_to_inventory(item_id: String) -> void:
+	for row in inventory:
+		for item_slot in row:
+			if item_slot == null:
+				if Items.items[item_id]:
+					item_slot = Items.items[item_id]
+				else:
+					print("WARNING: Attempted to add invalid item to inventory.")
+				print(inventory)
+				return

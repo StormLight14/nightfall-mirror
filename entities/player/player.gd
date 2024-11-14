74e74b2219959010d7437ce8a976aa4049dd5d7f
stormyyy <stormyyy@noreply.codeberg.org>
2024-11-14 08:45:14 -0700

    create Settings global, show label on pickupable and hide label on not

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -4,12 +4,27 @@ extends CharacterBody2D
 
 @onready var night_light = %NightLight
 
+var pickupable_object = null
+
 func _physics_process(delta: float) -> void:
+	movement(delta)
+	
+func movement(delta: float) -> void:
 	var input_vector := Input.get_vector("left", "right", "up", "down")
 	var direction := input_vector.normalized()
 	velocity = direction * SPEED * delta
 	move_and_slide()
 
 func _on_pickup_area_area_entered(area: Area2D) -> void:
-	print("pickup area entered")
-	area.get_parent().get_node("Hint").visible = true
+	var hint := area.get_parent().get_node("Hint")
+	if hint && Settings.show_hints:
+		hint.visible = true
+		
+	pickupable_object = area.get_parent()
+
+func _on_pickup_area_area_exited(area: Area2D) -> void:
+	var hint := area.get_parent().get_node("Hint")
+	if hint:
+		hint.visible = false
+		
+	pickupable_object = null

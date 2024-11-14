e298048a12d9ba9d140080da3cf5e0ff56aa3375
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-13 19:53:46 -0700

    kind of set up day/night cycle

--- scenes/main/island.gd
+++ scenes/main/island.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+var is_day = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	%Grass.update_full_tileset()
 	%Path.update_full_tileset()
 	daytime()
+	%AnimationPlayer.speed_scale = 15
+	%AnimationPlayer.play("turn_night")
 	pass
 	
 func _process(_delta) -> void:
@@ -19,10 +22,17 @@ func _process(_delta) -> void:
 
 func daytime() -> void:
 	%Sun.energy = 0.5
-	%Player.night_light.visible = false
 	%LevelLighting.color = Color(0.5, 0.5, 0.5, 1.0)
 
 func nighttime() -> void:
 	%Sun.energy = 0.0
-	%Player.night_light.visible = true
 	%LevelLighting.color = Color(0.0, 0.0, 0.0, 1.0)
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "turn_day":
+		%AnimationPlayer.play("turn_night")
+	elif anim_name == "turn_night":
+		%DaylightCycleTimer.start()
+
+func _on_daylight_cycle_timer_timeout() -> void:
+	%AnimationPlayer.play("turn_day")

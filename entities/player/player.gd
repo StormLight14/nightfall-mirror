9d44ad56286c105afe1fd5d52ba290bb2de8ffd6
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-13 18:35:49 -0700

    create helper functions for setting game to full day and full night

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var SPEED := 3000.0
 
+@onready var night_light = %NightLight
+
 func _physics_process(delta: float) -> void:
 	var input_vector := Input.get_vector("left", "right", "up", "down")
 	var direction := input_vector.normalized()
--- scenes/main/island.gd
+++ scenes/main/island.gd
@@ -5,4 +5,21 @@ extends Node2D
 func _ready() -> void:
 	%Grass.update_full_tileset()
 	%Path.update_full_tileset()
+	daytime()
 	pass
+	
+func _process(_delta) -> void:
+	if Input.is_action_just_pressed("turn_day"):
+		daytime()
+	elif Input.is_action_just_pressed("turn_night"):
+		nighttime()
+
+func daytime() -> void:
+	%Sun.energy = 0.5
+	%Player.night_light.visible = false
+	%LevelLighting.color = Color(0.5, 0.5, 0.5, 1.0)
+
+func nighttime() -> void:
+	%Sun.energy = 0.0
+	%Player.night_light.visible = true
+	%LevelLighting.color = Color(0.0, 0.0, 0.0, 1.0)

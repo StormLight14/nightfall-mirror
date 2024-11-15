a32f9e384309e33e68e17e3f459b95e816cb07df
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-14 19:50:29 -0700

    make Player.inventory array single dimensional

--- globals/Player.gd
+++ globals/Player.gd
@@ -1,18 +1,17 @@
 extends Node
 
-var inventory := [
-	[null, null, null, null, null],
-	[null, null, null, null, null],
-	[null, null, null, null, null]
-]
+var inventory := []
+
+func _ready():
+	for _i in range(16):
+		inventory.push_back(null)
 
 func add_to_inventory(item_id: String) -> void:
-	for row in inventory:
-		for i in range(len(row)):
-			if row[i] == null:
-				if Items.items.has(item_id):
-					row[i] = Items.items[item_id]  # assign item slot to have item dictionary
-				else:
-					print("WARNING: Attempted to add invalid item to inventory.")
-				print(inventory)
-				return
+	for i in range(len(inventory)):
+		if inventory[i] == null:
+			if Items.items.has(item_id):
+				inventory[i] = Items.items[item_id]  # assign item slot to have item dictionary
+			else:
+				print("WARNING: Attempted to add invalid item to inventory.")
+			print(inventory)
+			return
--- ui/inventory/inventory_slot.gd
+++ ui/inventory/inventory_slot.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var is_hotbar_slot = false
+@export var slot_id = 0
 
 func _ready():
 	if is_hotbar_slot:

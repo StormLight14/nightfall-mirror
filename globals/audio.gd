a9c384a562886c2a97124593fa73587ae86b6eef
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-15 18:44:09 -0700

    add "plop" sound when picking up an item

--- globals/audio.gd
+++ globals/audio.gd
@@ -1,9 +1,11 @@
 extends AudioStreamPlayer
 
-func play_sound(sound_name: String, at: float = 0.0) -> void:
+func play_sound(sound_name: String, at: float = 0.0, audio_scale: float = 1.0) -> void:
 	match sound_name:
 		"ui_click_1":
-			stream = load("res://ui/sounds/ui_click_1.mp3")
-
+			stream = load("res://sounds/ui_click_1.mp3")
+		"pickup":
+			print("pickup")
+			stream = load("res://sounds/pickup.mp3")
 	if stream:
 		play(at)

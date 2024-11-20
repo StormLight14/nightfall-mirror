3b2e345a5fe4611a8a1c357803edf0fd3c9da7c5
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-19 21:19:47 -0700

    make player light radius smaller; create shapeshifter.gd

--- entities/stalker/stalker.gd
+++ entities/stalker/stalker.gd
@@ -8,12 +8,20 @@ enum {
 }
 
 const FOLLOW_SPEED := 500.0
-const ATTACK_SPEED := 4500.0
+const ATTACK_SPEED := 4250.0
 
 var state := STILL
 var player: CharacterBody2D = null
+var paused := false
 
 func _physics_process(delta: float) -> void:
+	handle_states(delta)
+	
+	if paused:
+		velocity = Vector2.ZERO
+	move_and_slide()
+	
+func handle_states(delta: float):
 	var direction := Vector2.ZERO
 	if player:
 		direction = (player.global_position - global_position).normalized()
@@ -24,10 +32,9 @@ func _physics_process(delta: float) -> void:
 		FOLLOWING:
 			velocity = direction * FOLLOW_SPEED * delta
 		FLEEING:
-			velocity = -direction * ATTACK_SPEED * 1.2 * delta
+			velocity = -direction * ATTACK_SPEED * 3 * delta
 		ATTACKING:
 			velocity = direction * ATTACK_SPEED * delta
-	move_and_slide()
 
 func _on_follow_area_body_entered(body: Node2D) -> void:
 	player = body
@@ -53,8 +60,14 @@ func _on_attack_area_body_exited(body: Node2D) -> void:
 func _on_attack_timer_timeout() -> void:
 	if state == ATTACKING:
 		state = FLEEING
-		%FleeTimer.start()
+		paused = true
+		%PauseTimer.start() # stalker pauses before fleeing
 
 func _on_flee_timer_timeout() -> void:
 	if state == FLEEING:
 		state = STILL
+
+
+func _on_pause_timer_timeout() -> void:
+	%FleeTimer.start()
+	paused = false

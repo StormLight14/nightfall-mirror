9a7e468ab7b54899f43f642d64b47e0bf4070d77
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-26 16:22:35 -0700

    remove messy tiles; give trees slightly random scale

--- scenes/objects/trees/tree.gd
+++ scenes/objects/trees/tree.gd
@@ -4,4 +4,6 @@ func _ready():
 	var pine_type = randi_range(1, 2)
 	$Sprite2D.texture = load("res://scenes/objects/trees/pine" + str(pine_type) + ".png")
 	if pine_type == 2:
-		$Sprite2D.scale *= 1.5
+		scale *= 1.5
+	
+	scale *= randf_range(0.9, 1.1)

70f0b9df7895ada04ea6d2f741b687affd8d5e65
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-26 20:17:51 -0700

    remove wakatime plugin's generated readme, add my own

--- entities/evil_unicorn/evil_unicorn.gd
+++ entities/evil_unicorn/evil_unicorn.gd
@@ -1,5 +1,11 @@
 extends CharacterBody2D
 
+func _ready() -> void:
+	Signals.night_ended.connect(despawn)
+
 func _physics_process(delta: float) -> void:
 	for player in get_tree().get_nodes_in_group("Player"):
 		$Sprite2D.flip_h = player.global_position < global_position
+
+func despawn() -> void:
+	queue_free()

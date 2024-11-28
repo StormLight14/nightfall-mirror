fb2c789908d1b98571dc839ec3dc6a60a2ace72c
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-28 11:57:44 -0700

    add basic rain shader; interact with evil unicorn

--- entities/evil_unicorn/evil_unicorn.gd
+++ entities/evil_unicorn/evil_unicorn.gd
@@ -1,20 +1,28 @@
 extends CharacterBody2D
 
+var can_interact := false
+
 func _ready() -> void:
 	Signals.night_ended.connect(despawn)
 
 func _physics_process(delta: float) -> void:
 	for player in get_tree().get_nodes_in_group("Player"):
 		$Sprite2D.flip_h = player.global_position < global_position
+	
+	if Input.is_action_just_pressed("interact") && can_interact:
+		$Dialogue.text = "Have fun."
+		$InteractTimer.start()
 
 func despawn() -> void:
 	queue_free()
 
 func _on_interact_area_body_entered(body: Node2D) -> void:
 	$Dialogue.text = "Press E to play my game."
+	can_interact = true
 
 func _on_interact_area_body_exited(body: Node2D) -> void:
 	$Dialogue.text = ""
+	can_interact = false
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	$DespawnTimer.start()

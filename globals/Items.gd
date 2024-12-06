d6ac3fa30f11999a990888ffc8658310e20a3648
stormyyy <stormyyy@noreply.codeberg.org>
2024-12-06 08:55:38 -0700

    create rock on stick item

--- globals/Items.gd
+++ globals/Items.gd
@@ -6,20 +6,27 @@ var items := {
 		"display_name": "Rock",
 		"item_sprite": preload("res://scenes/objects/rocks/rock1.png"),
 		"stack_size": 10,
-		"crafting_ingredients": [null, 0]
+		"crafting_ingredients": null
 	},
 	"twig": {
 		"id": "twig",
 		"display_name": "Twig",
 		"item_sprite": preload("res://scenes/objects/items/twig.png"),
 		"stack_size": 10,
-		"crafting_ingredients": [null, 0]
+		"crafting_ingredients": null
 	},
 	"wood": {
 		"id": "wood",
 		"display_name": "Wood",
 		"item_sprite": preload("res://scenes/objects/items/wood.png"),
 		"stack_size": 10,
-		"crafting_ingredients": [null, 0]
+		"crafting_ingredients": null
 	},
+	"rock_on_stick": {
+		"id": "rock_on_stick",
+		"display_name": "Rock on Stick",
+		"item_sprite": preload("res://scenes/objects/items/rock_on_stick.png"),
+		"stack_size": 1,
+		"crafting_ingredients": [["rock", 1], ["twig", 1]]
+	}
 }
